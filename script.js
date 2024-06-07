14ef72973c013ff12b3285758b1362f595c34eff
hannahhous <127235968+hannahhous@users.noreply.github.com>
2024-06-07 02:46:23 -0400

    more personalized

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var menuButton = document.getElementById('menuButton');
-  var sideMenu = document.getElementById('sideMenu');
-
-  menuButton.addEventListener('click', function () {
-    sideMenu.classList.toggle('sideMenuShow');
-  });
-});
